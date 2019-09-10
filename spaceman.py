392fc0e2935c86d7da60974a659b2d42302d5bfe
Shaan <Shaan@admins-MacBook-Pro.local>
2019-09-09 22:12:53 -0700

    adding user input to to spaceman function and adding nested conditional loop to get gussed word fucntion

--- spaceman.py
+++ spaceman.py
@@ -1,5 +1,6 @@
 from random import choice
 
+
 def load_word():
     '''
     A function that reads a text file of words and randomly selects one to use as the secret word
@@ -14,6 +15,7 @@ def load_word():
     return secret_word
     pass
 
+
 def is_word_guessed(secret_word, letters_guessed):
     '''
     A function that checks if all the letters of the secret word have been guessed.
@@ -23,7 +25,6 @@ def is_word_guessed(secret_word, letters_guessed):
     Returns:
         bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
     '''
-
 
     # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
     # i = 0
@@ -38,7 +39,7 @@ def is_word_guessed(secret_word, letters_guessed):
         if letter not in letters_guessed:
             return False
     return True
-     pass
+    pass
 
 
 def get_guessed_word(secret_word, letters_guessed):
@@ -50,8 +51,14 @@ def get_guessed_word(secret_word, letters_guessed):
     Returns:
         string: letters and underscores.  For letters in the word that the user has guessed correctly, the string should contain the letter at the correct position.  For letters in the word that the user has not yet guessed, shown an _ (underscore) instead.
     '''
-
-    #TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
+    guessed_word = ""
+    for letter in secret_word:
+        if letter in letters_guessed:
+            guessed_word += letter
+        else:
+            guessed_word += "_"
+    return guessed_word
+    # TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
     # This loop will go through the secret word and count all the letters in the string and replace it with  a '_' symbol
     pass
 
@@ -65,11 +72,10 @@ def is_guess_in_word(guess, secret_word):
     Returns:
         bool: True if the guess is in the secret_word, False otherwise
     '''
-    #TODO: check if the letter guess is in the secret word
+    # TODO: check if the letter guess is in the secret word
+
 
     pass
-
-
 
 
 def spaceman(secret_word):
@@ -79,23 +85,29 @@ def spaceman(secret_word):
       secret_word (string): the secret word to guess.
     '''
 
+    while True:
+        print("Guess letter")
+        guess = input("guess here: ")
+        if len(guess) != 1:
+            print('Please enter a single letter.')
+        elif guess in letters_guessed:
+            print('You have already guessed that letter. Choose again.')
+        elif guess not in secret_word:
+            print('Please enter a LETTER.')
+        else:
+            return guess
+
     pass
-    #TODO: show the player information about the game according to the project spec
+    # TODO: show the player information about the game according to the project spec
 
-    #TODO: Ask the player to guess one letter per round and check that it is only one letter
+    # TODO: Ask the player to guess one letter per round and check that it is only one letter
 
-    #TODO: Check if the guessed letter is in the secret or not and give the player feedback
+    # TODO: Check if the guessed letter is in the secret or not and give the player feedback
 
-    #TODO: show the guessed word so far
+    # TODO: show the guessed word so far
 
-    #TODO: check if the game has been won or lost
+    # TODO: check if the game has been won or lost
 
-#
-# if __name__ == "__main__":
-#     print(is_word_guessed('hello', 'hellw'))
-
-
-
-    #These function calls that will start the game
+    # These function calls that will start the game
 secret_word = load_word()
 spaceman(secret_word)
