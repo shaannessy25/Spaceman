24a38888ac2b20d6b662456de0ca870eebd13d0f
Shaan <Shaan@admins-MacBook-Pro.local>
2019-09-18 19:16:29 -0700

    adding some more asserts in guess in word function

--- test_spaceman.py
+++ test_spaceman.py
@@ -9,3 +9,10 @@ def test_get_guessed_word ():
     assert get_guessed_word('hello',['h', 'e']) == "he_ _ _ "
     assert get_guessed_word('world', ['h', 'e', 's', 'q']) == '_ _ _ _ _ '
     assert get_guessed_word('world', ['h', 'e', 's', 'l']) == '_ _ _ l_ '
+
+def is_guess_in_word ():
+    assert is_guess_in_word('h', 'hello') == True
+    assert is_guess_in_word('h', 'air') == False
+    assert is_guess_in_word(['h', 'e', 'o'], 'hello') == True
+    assert is_guess_in_word(['h', 's', 'r'], 'hello') == True
+    assert is_guess_in_word(['w', 's', 'r'], 'hello') == False
